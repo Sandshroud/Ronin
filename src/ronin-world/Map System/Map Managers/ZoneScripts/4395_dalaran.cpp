2ee8748cdbd38cebb449569469ab9c7816146471
Thetruecrow <Crow5736@yahoo.com>
2023-04-28 23:00:03 -0500

    Cmake requires minimum version checks before defining project data, and older versions are deprecated(minimum 2.8.12). Restructured dalaran teleportation information inside of script.

--- src/ronin-world/Map System/Map Managers/ZoneScripts/4395_dalaran.cpp
+++ src/ronin-world/Map System/Map Managers/ZoneScripts/4395_dalaran.cpp
@@ -20,17 +20,27 @@
 #include "StdAfx.h"
 #include "4395_dalaran.h"
 
+DalaranTeleportLoc teleportLocs[DAL_TELEPORT_LOC_END] = 
+{
+    // DAL_TELEPORT_LOC_CAVERNS_OF_TIME
+    { 1, -8164.8f, -4768.5f, 34.3f, 0.f },
+    // DAL_TELEPORT_LOC_PURPLE_PARLOR
+    { 571, 5848.48f, 853.706f, 843.182f, 0.f },
+    // DAL_TELEPORT_LOC_VIOLET_CITADEL
+    { 571, 5819.26f, 829.774f, 680.22f, 0.f }
+};
+
 bool PortalEffectCavernsOfTime(SpellEffectClass *spell, uint32 effIndex, WorldObject *target, int32 amount, uint32 &mapId, float &X, float &Y, float &Z, float &O)
 {
     Player *plrTarget = target->IsPlayer() ? castPtr<Player>(target) : NULL;
     if(plrTarget == NULL)
         return false;
 
-    mapId = 1;
-    X = -8164.8f;
-    Y = -4768.5f;
-    Z = 34.3f;
-    O = 0.0f;
+    mapId = teleportLocs[DAL_TELEPORT_LOC_CAVERNS_OF_TIME].mapId;
+    X = teleportLocs[DAL_TELEPORT_LOC_CAVERNS_OF_TIME].x;
+    Y = teleportLocs[DAL_TELEPORT_LOC_CAVERNS_OF_TIME].y;
+    Z = teleportLocs[DAL_TELEPORT_LOC_CAVERNS_OF_TIME].z;
+    O = teleportLocs[DAL_TELEPORT_LOC_CAVERNS_OF_TIME].o;
     return true;
 }
 
@@ -40,11 +50,11 @@ bool PortalEffectPurpleParlor(SpellEffectClass *spell, uint32 effIndex, WorldObj
     if(plrTarget == NULL)
         return false;
 
-    mapId = 571;
-    X = 5848.48f;
-    Y = 853.706f;
-    Z = 843.182f;
-    O = 0.0f;
+    mapId = teleportLocs[DAL_TELEPORT_LOC_PURPLE_PARLOR].mapId;
+    X = teleportLocs[DAL_TELEPORT_LOC_PURPLE_PARLOR].x;
+    Y = teleportLocs[DAL_TELEPORT_LOC_PURPLE_PARLOR].y;
+    Z = teleportLocs[DAL_TELEPORT_LOC_PURPLE_PARLOR].z;
+    O = teleportLocs[DAL_TELEPORT_LOC_PURPLE_PARLOR].o;
     return true;
 }
 
@@ -54,19 +64,19 @@ bool PortalEffectVioletCitadel(SpellEffectClass *spell, uint32 effIndex, WorldOb
     if(plrTarget == NULL)
         return false;
 
-    mapId = 571;
-    X = 5819.26f;
-    Y = 829.774f;
-    Z = 680.22f;
-    O = 0.0f;
+    mapId = teleportLocs[DAL_TELEPORT_LOC_VIOLET_CITADEL].mapId;
+    X = teleportLocs[DAL_TELEPORT_LOC_VIOLET_CITADEL].x;
+    Y = teleportLocs[DAL_TELEPORT_LOC_VIOLET_CITADEL].y;
+    Z = teleportLocs[DAL_TELEPORT_LOC_VIOLET_CITADEL].z;
+    O = teleportLocs[DAL_TELEPORT_LOC_VIOLET_CITADEL].o;
     return true;
 }
 
 void SpellManager::_RegisterDalaranScripts()
 {
     // Portal effects in Dalaran
-    _RegisterTeleportData(59901, SP_EFF_INDEX_0, &PortalEffectCavernsOfTime);
-    _RegisterTeleportData(61419, SP_EFF_INDEX_0, &PortalEffectPurpleParlor);
-    _RegisterTeleportData(61420, SP_EFF_INDEX_0, &PortalEffectVioletCitadel);
+    _RegisterTeleportData(DAL_TELEPORT_SPELL_CAVERNS_OF_TIME, SP_EFF_INDEX_0, &PortalEffectCavernsOfTime);
+    _RegisterTeleportData(DAL_TELEPORT_SPELL_PURPLE_PARLOR, SP_EFF_INDEX_0, &PortalEffectPurpleParlor);
+    _RegisterTeleportData(DAL_TELEPORT_SPELL_VIOLET_CITADEL, SP_EFF_INDEX_0, &PortalEffectVioletCitadel);
 
 }
--- src/ronin-world/Map System/Map Managers/ZoneScripts/4395_dalaran.h
+++ src/ronin-world/Map System/Map Managers/ZoneScripts/4395_dalaran.h
@@ -19,3 +19,24 @@
 
 #pragma once
 
+enum DalaranTeleportSpellIds
+{
+    DAL_TELEPORT_SPELL_CAVERNS_OF_TIME = 59901,
+    DAL_TELEPORT_SPELL_PURPLE_PARLOR = 61419,
+    DAL_TELEPORT_SPELL_VIOLET_CITADEL = 61420
+};
+
+enum DalaranTeleportLocations
+{
+    DAL_TELEPORT_LOC_CAVERNS_OF_TIME,
+    DAL_TELEPORT_LOC_PURPLE_PARLOR,
+    DAL_TELEPORT_LOC_VIOLET_CITADEL,
+    DAL_TELEPORT_LOC_END
+};
+
+struct DalaranTeleportLoc
+{
+    uint32 mapId;
+    float x, y, z, o;
+};
+
